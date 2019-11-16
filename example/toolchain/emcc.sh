f49286a12a52acc43673911b7d2f4918fc6a4fa1
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-11-16 12:33:26 -0500

    sed bds again

--- example/toolchain/emcc.sh
+++ example/toolchain/emcc.sh
@@ -25,7 +25,7 @@ emcc -s EMIT_EMSCRIPTEN_METADATA=1 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS=['
 # Remove the first line of .d file
 # not sure why... https://docs.bazel.build/versions/master/tutorial/cc-toolchain-config.html
 # also, sorten the prefix as it seems that our clang doesn't support -fno-canonical-system-headers
-find . -name "*.d" -exec sed -i -e '2d' -e 's%[^ ]*/external/emscripten_toolchain/upstream/emscripten/system/%external/emscripten_toolchain/upstream/emscripten/system/%' {} \;
+find . -name "*.d" -exec sed -i '' -e '2d' -e 's%[^ ]*/external/emscripten_toolchain/upstream/emscripten/system/%external/emscripten_toolchain/upstream/emscripten/system/%' {} \;
 
 # yet another hack till i can figure out how to make no-canonical-prefixes work
-find . -name "*.d" -exec sed -i -e 's%[^ ]*/external/envoy_wasm-tmp/api/wasm/cpp%external/envoy_wasm%' {} \;
+find . -name "*.d" -exec sed -i '' -e 's%[^ ]*/external/envoy_wasm-tmp/api/wasm/cpp%external/envoy_wasm%' {} \;
