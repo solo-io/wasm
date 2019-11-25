23cf8bb99ef42b2ed02d4bd74f907de152eee391
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-11-25 13:56:35 -0500

    use sed just for the specific .d file

--- example/toolchain/emcc.sh
+++ example/toolchain/emcc.sh
@@ -5,10 +5,18 @@ set -euo pipefail
 
 emcc -s EMIT_EMSCRIPTEN_METADATA=1 -s STANDALONE_WASM=1 -s EXPORTED_FUNCTIONS=['_malloc','_free'] "$@"
 
-# Remove the first line of .d file
-# not sure why... https://docs.bazel.build/versions/master/tutorial/cc-toolchain-config.html
-# also, sorten the prefix as it seems that our clang doesn't support -fno-canonical-system-headers
-find . -name "*.d" -exec sed -i '' -e '2d' -e 's%[^ ]*/external/emscripten_toolchain/upstream/emscripten/system/%external/emscripten_toolchain/upstream/emscripten/system/%' {} \;
+# clang doesn't support `-no-canonical-system-headers` so sed it
+# find the .d file in the args and fix it:
 
-# yet another hack till i can figure out how to make no-canonical-prefixes work
-find . -name "*.d" -exec sed -i '' -e 's%[^ ]*/external/envoy_wasm_api-tmp/api/wasm/cpp%external/envoy_wasm_api%' {} \;
+for arg in "$@"
+do
+    if [ ${arg: -2} == ".d" ]; then
+        echo Fixing $arg
+        sed -e 's%[^ ]*/external/emscripten_toolchain/upstream/emscripten/system/%external/emscripten_toolchain/upstream/emscripten/system/%' $arg > $arg.tmp
+        mv $arg.tmp $arg
+        break
+    fi
+done
+
+
+ 
