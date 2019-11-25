b847954046540e09df7cce3aafc18663981fa973
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-11-25 11:40:42 -0500

    don't hard code

--- test-loader/main.cc
+++ test-loader/main.cc
@@ -6,12 +6,19 @@
 #include "test/mocks/event/mocks.h"
 #include "test/mocks/upstream/mocks.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+
+  if (args != 2) {
+    std::cerr << "please provide path to wasm file" << std::endl;
+    exit(1);
+  }
+
+  const char* wasm_module = argv[1];
 
   NiceMock<Envoy::Event::MockDispatcher> dispatcher;
   NiceMock<Envoy::Upstream::MockClusterManager> cluster_manager;
   Envoy::Stats::IsolatedStoreImpl stats_store;
-  std::ifstream t("../example/bazel-bin/envoy_filter_http_wasm_example.wasm");
+  std::ifstream t(wasm_module);
   std::stringstream buffer;
   buffer << t.rdbuf();
   Envoy::Extensions::Common::Wasm::Wasm vm("envoy.wasm.runtime.v8", "vm_id", "vm_configuration",
@@ -20,4 +27,6 @@ int main() {
     std::cerr << "error init" << std::endl;
     exit(1);
   }
+  std::cout << "loaded successfully" << std::endl;
+  return 0;
 }
