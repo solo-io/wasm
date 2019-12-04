e26d79aef9bdee2c657f019236952de79ae05fbd
Eitan Yarmush <eitan.yarmush@solo.io>
2019-12-04 17:40:55 -0500

    Rust (#1)
    * rust example start
    * removed binary
    * moving to linux
    * will run in envoy now
    * new rust code
    * technically got serde working, still running into memory issue though
    * basic rust is good
    * mods

---
+++ example/rust/README.md
@@ -0,0 +1,8 @@
+# Compiling rust to .wasm files using the SDK
+
+The rust SDK is a WIP.
+
+## rust if you want to use it or rebuild the rust WebAssembly tests
+
+curl https://sh.rustup.rs -sSf | sh
+
---
+++ example/rust/build.rs
@@ -0,0 +1,16 @@
+// extern crate protoc_rust;
+
+// use protoc_rust::Customize;
+
+fn main() {
+	// protoc_rust::Args::new()
+	// 	.out_dir("src")
+	// 	.inputs(&["proto/filter.proto"])
+	// 	.include(".")    
+	// 	.customize(Customize {
+	// 		serde_derive: Some(true),
+	// 		..Default::default()
+	// 	})
+	// 	.run()
+	// 	.expect("protoc");
+}
