3201b192344b08ee74e2997faf38773ee6bc4adc
Scott Weiss <sdw35@cornell.edu>
2020-01-13 17:36:59 -0500

    implement wasme operator, add generation script

---
+++ operator/api/autopilot_generate.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+
+	"github.com/solo-io/autopilot/codegen"
+	"github.com/solo-io/autopilot/codegen/model"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+func main() {
+	cmd := &codegen.Command{
+		Groups: []model.Group{
+			{
+				ProtoDir: "operator/api",
+				GroupVersion: schema.GroupVersion{
+					Group:   "wasme.io",
+					Version: "v1",
+				},
+				Module: "github.com/solo-io/wasme",
+				Resources: []model.Resource{
+					{
+						Kind: "FilterDeployment",
+						Spec: model.Field{
+							Type: "FilterDeploymentSpec",
+						},
+						Status: &model.Field{
+							Type: "FilterDeploymentStatus",
+						},
+					},
+				},
+				RenderProtos:     true,
+				RenderManifests:  true,
+				RenderTypes:      true,
+				RenderClients:    true,
+				RenderController: true,
+				ApiRoot:          "operator/pkg/api",
+			},
+		},
+		ManifestRoot: "operator/install/kube",
+	}
+
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("executed generation with opts: %v", cmd)
+}
---
+++ pkg/operator/operator.go
@@ -0,0 +1,135 @@
+package operator
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/solo-io/autopilot/pkg/ezkube"
+	v1 "github.com/solo-io/wasme/operator/pkg/api/wasme.io/v1"
+	"github.com/solo-io/wasme/pkg/deploy/istio"
+	"github.com/solo-io/wasme/pkg/pull"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type filterDeploymentHandler struct {
+	ctx context.Context
+
+	kubeClient kubernetes.Interface
+	client     ezkube.Ensurer
+
+	puller pull.CodePuller
+	cache  istio.Cache
+}
+
+func (f *filterDeploymentHandler) Create(obj *v1.FilterDeployment) error {
+	return f.deploy(obj)
+}
+
+func (f *filterDeploymentHandler) Update(_, obj *v1.FilterDeployment) error {
+	return f.deploy(obj)
+}
+
+func (f *filterDeploymentHandler) Delete(obj *v1.FilterDeployment) error {
+	//for _, dep := range
+	return f.undeploy(obj)
+}
+
+func (f *filterDeploymentHandler) Generic(obj *v1.FilterDeployment) error {
+	// should never be called
+	panic("not implemented")
+}
+
+func (f *filterDeploymentHandler) deploy(obj *v1.FilterDeployment) error {
+	status := v1.FilterDeploymentStatus{
+		ObservedGeneration: obj.Generation,
+		Workloads:          map[string]*v1.WorkloadStatus{},
+	}
+
+	err := f.handleFilter(obj, false, func(workloadMeta metav1.ObjectMeta, err error) {
+		workloadStatus := &v1.WorkloadStatus{
+			State: v1.WorkloadStatus_Succeeded,
+		}
+		if err != nil {
+			workloadStatus = &v1.WorkloadStatus{
+				Reason: err.Error(),
+				State:  v1.WorkloadStatus_Failed,
+			}
+		}
+		status.Workloads[obj.Name] = workloadStatus
+	})
+
+	if err != nil {
+		status.Reason = err.Error()
+	}
+
+	obj.Status = status
+
+	return f.client.UpdateStatus(f.ctx, obj)
+}
+
+func (f *filterDeploymentHandler) undeploy(obj *v1.FilterDeployment) error {
+	return f.handleFilter(obj, true, nil)
+}
+
+func getFilter(obj *v1.FilterDeployment) (*v1.FilterSpec, error) {
+	filter := obj.Spec.GetFilter()
+	if filter == nil {
+		return nil, errors.Errorf("must provide spec.filter")
+	}
+	if filter.Id == "" {
+		filter.Id = obj.Name + "." + obj.Namespace
+	}
+	return filter, nil
+}
+
+func getDeployment(obj *v1.FilterDeployment) (*v1.DeploymentSpec, error) {
+	deployment := obj.Spec.GetDeployment()
+	if deployment == nil {
+		return nil, errors.Errorf("must provide spec.deployment")
+	}
+	return deployment, nil
+}
+
+func (f *filterDeploymentHandler) handleFilter(obj *v1.FilterDeployment, remove bool, onWorkload func(workloadMeta metav1.ObjectMeta, err error)) error {
+	filter, err := getFilter(obj)
+	if err != nil {
+		return err
+	}
+
+	deployment, err := getDeployment(obj)
+	if err != nil {
+		return err
+	}
+
+	switch dep := deployment.GetDeploymentType().(type) {
+	case *v1.DeploymentSpec_Istio:
+		workload := istio.Workload{
+			Kind:      dep.Istio.GetKind(),
+			Name:      dep.Istio.GetName(),
+			Namespace: obj.Namespace,
+		}
+
+		provider, err := istio.NewProvider(
+			f.ctx,
+			f.kubeClient,
+			f.client,
+			f.puller,
+			workload,
+			f.cache,
+			obj,
+			onWorkload,
+		)
+		if err != nil {
+			return err
+		}
+
+		if remove {
+			return provider.RemoveFilter(filter)
+		} else {
+			return provider.ApplyFilter(filter)
+		}
+	default:
+		return errors.Errorf("internal error: %T not implemented", deployment)
+	}
+}
