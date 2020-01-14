22176b10173fe43dc4d4136e4fd186321bf841d4
Scott Weiss <sdw35@cornell.edu>
2020-01-14 08:44:39 -0500

    fix import

--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/solo-io/autopilot/pkg/ezkube"
-	v1 "github.com/solo-io/wasme/operator/pkg/api/wasme.io/v1"
 	"github.com/solo-io/wasme/pkg/deploy/istio"
+	v1 "github.com/solo-io/wasme/pkg/operator/api/wasme.io/v1"
 	"github.com/solo-io/wasme/pkg/pull"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
