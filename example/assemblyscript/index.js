31e9c8aa81ff8c272f70d4d014d0e47396e6dc37
Scott Weiss <sdw35@cornell.edu>
2020-02-10 11:07:48 -0500

    remove as example

--- example/assemblyscript/index.js
+++
@@ -1,3 +0,0 @@
-const fs = require("fs");
-const loader = require("@assemblyscript/loader");
-module.exports = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), { /* imports */ })
