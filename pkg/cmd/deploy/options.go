ce5fea47902f2731d6206f6eef4f006551250d25
Scott Weiss <sdw35@cornell.edu>
2020-02-11 12:20:37 -0500

    fix wasme deploy istio command

--- pkg/cmd/deploy/options.go
+++ pkg/cmd/deploy/options.go
@@ -173,6 +173,13 @@ func (opts *options) makeProvider(ctx context.Context) (deploy.Provider, error) 
 			return nil, err
 		}
 
+		go func(){
+			err := mgr.Start(ctx.Done())
+			if err != nil {
+				log.Fatalf("failed to start kubernetes dynamic client")
+			}
+		}()
+
 		return istio.NewProvider(
 			ctx,
 			kubeClient,
