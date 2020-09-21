3f93933535dd9f0155432c13b87ccf0e7b3f2811
Shane O'Donnell <shane.odonnell@solo.io>
2020-09-21 19:26:07 -0400

    Fix tests insufficient CPU error

--- tools/wasme/cli/test/e2e/build/build_suite_test.go
+++ tools/wasme/cli/test/e2e/build/build_suite_test.go
@@ -1,20 +1,13 @@
 package build_test
 
 import (
-	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/solo-io/go-utils/log"
 )
 
 func TestBuild(t *testing.T) {
-	if os.Getenv("FILTER_BUILD_IMAGE_TAG") == "" {
-		log.Warnf("This test is disabled. " +
-			"To enable, set FILTER_BUILD_IMAGE_TAG in your env.")
-		return
-	}
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Build Suite")
 }
--- tools/wasme/cli/test/e2e/deploy/gloo/gloo_suite_test.go
+++ tools/wasme/cli/test/e2e/deploy/gloo/gloo_suite_test.go
@@ -1,20 +1,13 @@
 package gloo_test
 
 import (
-	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
-	"github.com/solo-io/go-utils/log"
 	"github.com/solo-io/solo-kit/test/helpers"
 )
 
 func TestOperator(t *testing.T) {
-	if os.Getenv("FILTER_IMAGE_GLOO_TAG") == "" {
-		log.Warnf("This test is disabled. " +
-			"To enable, set FILTER_IMAGE_GLOO_TAG to a valid gloo wasm filter image in your env.")
-		return
-	}
 	helpers.RegisterCommonFailHandlers()
 	helpers.SetupLog()
 	RunSpecs(t, "Deploy Gloo Suite")
