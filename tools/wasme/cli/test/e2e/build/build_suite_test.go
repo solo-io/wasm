eedd55bec07a6a0d7d28a6da145e6a86814d274c
Shane O'Donnell <shane.odonnell@solo.io>
2020-09-21 14:23:25 -0400

    Speed up e2e tests
    Stop running duplicate tests for Gloo in the Istio e2e suites

--- tools/wasme/cli/test/e2e/build/build_suite_test.go
+++ tools/wasme/cli/test/e2e/build/build_suite_test.go
@@ -5,9 +5,16 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/solo-io/go-utils/log"
+	"github.com/solo-io/wasm/tools/wasme/cli/test"
 )
 
 func TestBuild(t *testing.T) {
+	if test.GetBuildImageTag() == "" {
+		log.Warnf("This test is disabled. " +
+			"To enable, set FILTER_BUILD_IMAGE_TAG in your env.")
+		return
+	}
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Build Suite")
 }
--- tools/wasme/cli/test/e2e/deploy/gloo/gloo_suite_test.go
+++ tools/wasme/cli/test/e2e/deploy/gloo/gloo_suite_test.go
@@ -3,12 +3,18 @@ package gloo_test
 import (
 	"testing"
 
-	"github.com/solo-io/solo-kit/test/helpers"
-
 	. "github.com/onsi/ginkgo"
+	"github.com/solo-io/go-utils/log"
+	"github.com/solo-io/solo-kit/test/helpers"
+	"github.com/solo-io/wasm/tools/wasme/cli/test"
 )
 
 func TestOperator(t *testing.T) {
+	if test.GetImageTagGloo() == "" {
+		log.Warnf("This test is disabled. " +
+			"To enable, set FILTER_IMAGE_GLOO_TAG to a valid gloo wasm filter image in your env.")
+		return
+	}
 	helpers.RegisterCommonFailHandlers()
 	helpers.SetupLog()
 	RunSpecs(t, "Deploy Gloo Suite")
