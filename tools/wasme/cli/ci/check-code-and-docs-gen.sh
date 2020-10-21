77f82bdc3cc3eaa3bfa081ae849271c2c54bf9a0
Shane O'Donnell <shane.odonnell@solo.io>
2020-10-21 17:33:06 -0400

    Ignore diffs in tars for codegen (#207)
    * Ignore diffs in tars for codegen
    * Change generation order
    * Ignore gitignore
    * Output already gitignored

--- tools/wasme/cli/ci/check-code-and-docs-gen.sh
+++ tools/wasme/cli/ci/check-code-and-docs-gen.sh
@@ -4,22 +4,23 @@ set -ex
 
 protoc --version
 
-if [ ! -f .gitignore ]; then
-  echo "_output" > .gitignore
-fi
-
 make install-deps
 
 set +e
 
-make generated-code operator-gen manifest-gen -B > /dev/null
+make manifest-gen generated-code -B > /dev/null
 if [[ $? -ne 0 ]]; then
   echo "Code generation failed"
   exit 1;
 fi
+
+# Tars can build slightly differently based on the host system.
+# At build time, they will be re-built in CI, so we can ignore diffs in tars
+git status --porcelain | grep archive_2gobytes.go | cut -c 20- | xargs git restore
+
 if [[ $(git status --porcelain | wc -l) -ne 0 ]]; then
   echo "Error: Generating code produced a non-empty diff"
-  echo "Try running 'make install-go-tools generated-code operator-gen manifest-gen -B' from the tools/wasme/cli directory, then re-pushing."
+  echo "Try running 'make clean install-deps manifest-gen generated-code -B' from the tools/wasme/cli directory, then re-pushing."
   git status --porcelain
   git diff | cat
   exit 1;
