c803eb97ae6f551ec5d80d07c105b4f22ae6797b
Shane O'Donnell <shane.odonnell@solo.io>
2020-10-21 15:35:31 -0400

    Fix bad merge

--- tools/wasme/cli/ci/check-code-and-docs-gen.sh
+++ tools/wasme/cli/ci/check-code-and-docs-gen.sh
@@ -12,14 +12,14 @@ make install-deps
 
 set +e
 
-make generated-code operator-gen manifest-gen -B > /dev/null
+make manifest-gen generated-code -B > /dev/null
 if [[ $? -ne 0 ]]; then
   echo "Code generation failed"
   exit 1;
 fi
 if [[ $(git status --porcelain | wc -l) -ne 0 ]]; then
   echo "Error: Generating code produced a non-empty diff"
-  echo "Try running 'make install-go-tools generated-code operator-gen manifest-gen -B' from the tools/wasme/cli directory, then re-pushing."
+  echo "Try running 'make clean install-deps manifest-gen generated-code -B' from the tools/wasme/cli directory, then re-pushing."
   git status --porcelain
   git diff | cat
   exit 1;
