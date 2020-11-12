e209be526b659a824bbdad8412b6da269da8dbe0
Scott Weiss <sdw35@cornell.edu>
2020-11-12 11:05:46 -0600

    remove unused

--- tools/wasme/pkg/util/proto.go
+++ tools/wasme/pkg/util/proto.go
@@ -2,9 +2,6 @@ package util
 
 import (
 	"bytes"
-	gogojsonpb "github.com/gogo/protobuf/jsonpb"
-	"github.com/gogo/protobuf/types"
-	"github.com/rotisserie/eris"
 
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
@@ -25,24 +22,6 @@ func MarshalStruct(m proto.Message) (*structpb.Struct, error) {
 	return &pb, err
 }
 
-func MarshalGogoStruct(msg proto.Message) (*types.Struct, error) {
-	if msg == nil {
-		return nil, eris.New("nil message")
-	}
-
-	buf := &bytes.Buffer{}
-	if err := (&jsonpb.Marshaler{OrigName: true}).Marshal(buf, msg); err != nil {
-		return nil, err
-	}
-
-	pbs := &types.Struct{}
-	if err := gogojsonpb.Unmarshal(buf, pbs); err != nil {
-		return nil, err
-	}
-
-	return pbs, nil
-}
-
 func UnmarshalStruct(s *structpb.Struct, m proto.Message) error {
 	data, err := MarshalBytes(s)
 	if err != nil {
@@ -59,19 +38,4 @@ func MarshalBytes(pb proto.Message) ([]byte, error) {
 
 func UnmarshalBytes(b []byte, pb proto.Message) error {
 	return jsonpb.Unmarshal(bytes.NewBuffer(b), pb)
-}
-
-func StructPbToGogo(structuredData *structpb.Struct) (*types.Struct, error) {
-	if structuredData == nil {
-		return nil, eris.New("cannot unmarshal nil struct")
-	}
-	byt, err := proto.Marshal(structuredData)
-	if err != nil {
-		return nil, err
-	}
-	var st types.Struct
-	if err := proto.Unmarshal(byt, &st); err != nil {
-		return nil, err
-	}
-	return &st, nil
 }
